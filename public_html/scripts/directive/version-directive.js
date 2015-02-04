82dd3c794ea21f484da43b1147c7265c6f8408d0
Quirino Desktop <quirino@jamgling.com>
2015-02-04 03:48:48 +0100

    mancano direttive da implementare bene

--- public_html/scripts/directive/version-directive.js
+++ public_html/scripts/directive/version-directive.js
@@ -1,9 +1,41 @@
 'use strict';
 
-define(function () {
-    return ['VersionService', function(versionService) {
-        return function(scope, elm, attrs) {
-            elm.text(versionService.version);
+define(['main-starter'], function (mainStarter) {
+    
+    return jamgleApp.directive('appVersion', ['VersionService', function (VersionService) {
+            
+        var obj = {
+            restrict: 'E',
+            scope: {
+            },
+            replace: true,
+            controller: function($scope) {
+                
+                $scope.version= VersionService.version;
+                
+                $scope.init = function() {
+                    console.log("INIT VERSION DIRECTIVE CONTROLLER");
+                };
+
+                $scope.init();
+
+            },
+            templateUrl: 'views/partials/app-version.html'
         };
-    }];
+
+        obj.controller.$inject = ['$scope'];
+
+        return obj;
+
+    }]);
+
 });
+
+
+//define(function () {
+//    return ['VersionService', function(versionService) {
+//        return function(scope, elm, attrs) {
+//            elm.text(versionService.version);
+//        };
+//    }];
+//});
--- public_html/scripts/routes.js
+++ public_html/scripts/routes.js
@@ -3,10 +3,10 @@
 define(['main-starter'], function (app) {
     return app.config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/view1', {
-            templateUrl: 'partials/partial1.html',
+            templateUrl: 'views/partial1.html',
             controller: 'MyController1'});
         $routeProvider.when('/view2', {
-            templateUrl: 'partials/partial2.html',
+            templateUrl: 'views/partial2.html',
             controller: 'MyController2'});
         $routeProvider.otherwise(
             {redirectTo: '/view1'});
